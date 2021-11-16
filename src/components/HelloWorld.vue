14e01eeb6f604d06157d7a999153b2dc5c9d7591
RivaldoTherino <93975111+RivaldoTherino@users.noreply.github.com>
2021-11-17 02:21:40 +0700

    test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,120 @@
+<template>
+<div>
+<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Rivaldo Therino Elevan</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#EducationalBackground">Educational Background</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<section class="jumbotron text-center">
+  <img src="Profil.png" width="190" class="rounded-circle img-thumbnail">
+  <h1 class="display-4">Rivaldo Therino Elevan</h1>
+  <p class="lead">Employment | College Student</p>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffff" fill-opacity="1" d="M0,96L0,320L45,320L45,128L90,128L90,192L135,192L135,64L180,64L180,288L225,288L225,256L270,256L270,32L315,32L315,288L360,288L360,128L405,128L405,96L450,96L450,160L495,160L495,160L540,160L540,288L585,288L585,128L630,128L630,224L675,224L675,160L720,160L720,64L765,64L765,96L810,96L810,192L855,192L855,64L900,64L900,288L945,288L945,256L990,256L990,192L1035,192L1035,256L1080,256L1080,96L1125,96L1125,288L1170,288L1170,0L1215,0L1215,128L1260,128L1260,224L1305,224L1305,160L1350,160L1350,96L1395,96L1395,128L1440,128L1440,320L1395,320L1395,320L1350,320L1350,320L1305,320L1305,320L1260,320L1260,320L1215,320L1215,320L1170,320L1170,320L1125,320L1125,320L1080,320L1080,320L1035,320L1035,320L990,320L990,320L945,320L945,320L900,320L900,320L855,320L855,320L810,320L810,320L765,320L765,320L720,320L720,320L675,320L675,320L630,320L630,320L585,320L585,320L540,320L540,320L495,320L495,320L450,320L450,320L405,320L405,320L360,320L360,320L315,320L315,320L270,320L270,320L225,320L225,320L180,320L180,320L135,320L135,320L90,320L90,320L45,320L45,320L0,320L0,320Z"></path></svg>
+</section>
+
+<section id="about">
+  <div class="container">
+    <div class="row text-center mb-3" >
+      <div class="col">
+        <h2>About Me</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center fs-4 text-center">
+      <div class="col-md-4">
+        <p>Hallo teman, Nama saya Rivaldo Therino Elevan. Saya lahir di Palembang pada tanggal 11 november 2002, saya anak ke-4 dari 5 bersaudara Sekarang saya masih tinggal dikota tercinta Palembang.</p>
+      </div>
+      <div class="col-md-4">
+        <p>Sekarang saya sedang menempuh pendidikan perguruan tinggi di Universitas Multi Data Palembang. Senang berkenalan dengan Anda.</p>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#68686e" fill-opacity="1" d="M0,64L34.3,85.3C68.6,107,137,149,206,149.3C274.3,149,343,107,411,96C480,85,549,107,617,128C685.7,149,754,171,823,170.7C891.4,171,960,149,1029,138.7C1097.1,128,1166,128,1234,122.7C1302.9,117,1371,107,1406,101.3L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
+</section>
+
+<section id="EducationalBackground">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col1" style="color:white;" text>
+        <h2>My Educational Background</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center md-4">
+      <div class="col-md-4 mb-3 p-3">
+          <div class="card" style="width: 18rem;">
+            <img src="TK.png" width = "450" height="200" class="card-img-top" alt="Gambar1">
+              <div class="card-body">
+                  <p class="card-text">Ini adalah foto bangunan TK Methodist-2 Palembang. Saya menempuh masa taman kanak-kanak di TK ini.</p>
+              </div>
+          </div>
+      </div>
+      <div class="col-md-4 mb-3 p-3">
+          <div class="card" style="width: 18rem;">
+            <img src="SD.jpg" width = "450" height="200" class="card-img-top" alt="Gambar2">
+              <div class="card-body">
+                  <p class="card-text">Ini adalah Bangunan Sekolah Methodist-2 Palembang. Saya menempuh Pendidikan dari mulai SD hingga SMA di Sekolah ini.</p>
+              </div>
+          </div>
+      </div>
+      <div class="col-md-4 mb-3 p-3">
+          <div class="card" style="width: 18rem;">
+            <img src="Kuliah.png" width = "450" height="200" class="card-img-top" alt="Gambar3">
+              <div class="card-body">
+                  <p class="card-text">Ini adalah Bangunan Universitas Multi data Palembang. Saya sedang menempuh Pendidikan perguruan tinggi disini.</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<footer class="footer-dark bg-dark">
+  
+</footer>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+    .jumbotron{
+      color: white;
+      padding-top: 6rem;
+      background: rgb(110, 104, 104);
+    }
+    #EducationalBackground{
+      background: rgb(110, 104, 104);
+    }
+
+    .col1{
+      padding: 2rem;
+      border-style: double;
+    }
+    section{
+      padding-top: 5rem;
+    }
+</style>
