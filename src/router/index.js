7ab1e9e7d229fe98143e4e5e71270a741f580630
RivaldoTherino <93975111+RivaldoTherino@users.noreply.github.com>
2021-12-09 13:26:52 +0700

    Fetch API : GET data users

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,26 @@ const routes = [
     path: "/register",
     name: "register",
     component: () => import('@/views/register/Main.vue')
+},
+{
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import('@/views/dashboard/Main.vue')
+},
+{
+    path: '/profil',
+    name: 'profil',
+    component: () => import('@/views/profil/Main')
+},
+{
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/about/Main')
+},
+{
+    path: '/users',
+    name: 'users',
+    component: () => import('@/views/users/Main')
 }];
 const router = createRouter({
     history: createWebHistory(),
