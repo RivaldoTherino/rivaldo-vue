8d0de63cf8fdc302c07c4afab704441aafd9b71c
RivaldoTherino <93975111+RivaldoTherino@users.noreply.github.com>
2021-12-09 14:29:01 +0700

    Lanjutan Fetch API : POST

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,55 @@
-import{ createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
+
 const routes = [
-{
-    path: "/",
-    name: "home",
-    component: () => import('@/components/HelloWorld.vue')
-},
-{
-    path: "/login",
-    name: "login",
-    component: () => import('@/views/login/Main.vue')
-},
-{
-    path: "/register",
-    name: "register",
-    component: () => import('@/views/register/Main.vue')
-},
-{
-    path: "/dashboard",
-    name: "dashboard",
-    component: () => import('@/views/dashboard/Main.vue')
-},
-{
-    path: '/profil',
-    name: 'profil',
-    component: () => import('@/views/profil/Main')
-},
-{
-    path: '/about',
-    name: 'about',
-    component: () => import('@/views/about/Main')
-},
-{
-    path: '/users',
-    name: 'users',
-    component: () => import('@/views/users/Main')
-}];
+    {
+        path: "/",
+        name: "home",
+        component: () => import('@/components/HelloWorld')
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: () => import('@/views/login/Main')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import ('@/views/register/Main')
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard/Main')
+    },
+    {
+        path: '/profil',
+        name: 'profil',
+        component: () => import('@/views/profil/Main')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('@/views/about/Main')
+    },
+    {
+        path: '/users',
+        name: 'users',
+        component: () => import('@/views/users/Main')
+    },
+    {
+        path: '/users/:id/:username?',
+        name: 'detailuser',
+        component: () => import('@/views/users/DetailUser')
+    },
+    {
+        path: '/create-user',
+        name: 'createuser',
+        component: () => import('@/views/users/FormCreate')
+    },
+];
+
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
-})
+});
 export default router;
--- src/views/users/Main.vue
+++ src/views/users/Main.vue
@@ -3,22 +3,23 @@
         <layout-main>
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Users</h1>
+                <router-link 
+                        :to="{name:'createuser'}" 
+                        class="btn btn-success">Create User</router-link>
             </div>
-            <div class="row row-cols-1 row-cols-md-4 row-cols-sm-2 g-2">
-                <div v-for="user in datausers" :key="user.id" class="col card">
-                    <img src="default.pp.png" class="card-img-top" alt="...">
+            <div class="row row-cols-4 g-3">
+                <div v-for="user in datausers" :key="user" class="card col mr-1">
+                    <img src="/default.pp.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">{{ user.company.name }}</h5>
-                        <address class="card-text">
-                           <strong>{{ user.Username }}</strong>   
-                           <br>
-                            {{user.address.street}}, {{user.address.suite}}<br>
-                            {{user.address.city}}, {{user.address.zipcode}}<br>
-                            <abbr title="phone">{{user.phone}}</abbr><br/>
-                            <strong>{{user.username}}</strong> <br>
-                            {{user.email}}
-                        </address>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">{{ user.name }}</h5>
+                        <h6>{{user.username}}</h6>
+                        <p class="card-text">
+                            Company : {{ user.company.name }}
+                        </p>
+                        <router-link 
+                        :to="{name:'detailuser', 
+                        params: { id: user.id, username: user.username }}" 
+                        class="btn btn-primary">Detail User</router-link>
                     </div>
                 </div>
             </div>
@@ -31,27 +32,26 @@ import LayoutMain from "@/views/LayoutMain"
 export default {
     components: {
         LayoutMain
-    },
+    }, 
     data() {
         return {
-           datausers: null,
+            datausers: null
         }
     },
     methods: {
-        getUsers(){
+        loadUsers(){
             fetch("https://jsonplaceholder.typicode.com/users")
-                .then(response => response.json()) //then 1, set response sebagai json
+                .then(response => response.json())  //then 1, set response sebagai json
                 .then(json => {
-                    this.datausers = json;    
-                }) //then2, mengambil json dan ditampung/diset ke dalam data
+                    this.datausers = json;
+                })   //then 2, ambil respon json dan set sebagai data
                 .catch(error => {
                     console.log(error);
                 })
         }
-    },    
+    },
     mounted() {
-        console.log("HALLO");
-        this.getUsers();
+        this.loadUsers();
     }
 }
 </script>
